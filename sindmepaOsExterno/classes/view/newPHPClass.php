79f932ba9f6594caf4576b51d5c7530b743ca979
victor <victorps91@gmail.com>
2020-01-31 22:16:32 -0300

    alteração

--- sindmepaOsExterno/classes/view/newPHPClass.php
+++
@@ -1,7 +0,0 @@
-<div class="col-sm-4">
-    <label for="protocolo">Protocolo:</label>
-    <input name="protocolo" id="protocolo" type="text" class="form-control"  placeholder="Nr. do Protocolo"> 
-    <br/> 
-    <button type="submit" class="btn btn-success">Pesquisar</button>                     
-    <a class="btn btn-success" href="cadastro-protocolo.php" role="button">Novo Protocolo</a>
-</div>
